b9be69611088d83ed2cc4874c09b3ebbdcf2ff04
potapen <po@ta.pen>
2022-03-22 17:03:02 +0100

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,5 @@
 const CelebrityModel = require("../models/Celebrity.model");
+const { findByIdAndUpdate } = require("../models/Movie.model");
 const MovieModel = require("../models/Movie.model");
 
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
@@ -56,6 +57,40 @@ router.post('/:id/delete', async (request,response,next)=> {
         const movie = await MovieModel.findByIdAndDelete(id);
         console.log(movie);
         // response.send('deleted!');
+        response.redirect('/movies');
+    }
+    catch(error){console.log(error)}
+});
+
+router.get('/:id/edit', async (request,response,next)=> {
+    try{
+        const {id} = request.params;
+        const movie = await MovieModel.findById(id).populate('cast');
+        const celebrities = await CelebrityModel.find();
+        celebrities.forEach(person1 => {
+
+            const isSelected = movie.cast.some(person2 => {
+                const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
+                console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
+                return isSame
+            })
+            person1.selected = isSelected
+        })
+        response.render('movies/edit-movie', {movie, celebrities});
+    }
+    catch(error){console.log(error)}
+});
+
+
+router.post('/:id/edit', async (request,response,next)=> {
+    try{
+        const {id} = request.params;
+        console.log('id is: ', id);
+        const movie = request.body;
+        console.log(movie);
+        const updatedMovie = await MovieModel.findByIdAndUpdate(id, movie, {new: 'true'});
+        console.log(updatedMovie);
+        // response.send('edited!');
         response.redirect('/movies');
     }
     catch(error){console.log(error)}
